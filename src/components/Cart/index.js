7d27c838fd55f9e0a31c1d6847f4acb9c00c879b
anilthatikanti <thatikantianil@outlook.com>
2022-08-13 07:50:13 +0000

    E-commerce website v 0.0.1

---
+++ src/components/Cart/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div className="cartContainer">
+      <img
+        className="cartImage"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+      />
+    </div>
+  </>
+)
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="productContainer">
+      <img
+        className="productImage"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+      />
+    </div>
+  </>
+)
+export default Products
